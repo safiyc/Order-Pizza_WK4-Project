eccad2a9dabb188f4a52afb9367ff4e0f29f3e45
Safiy Cham <csafi13@yahoo.com>
2017-08-25 09:52:36 -0700

    add business logic and inital html template

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//business logic
+function Pizza (size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+}
+
+Pizza.prototype.getCost = function () {
+  this.toppings === 2;
+  if (this.size === 8 || this.size === 12 || this.size === 15) {
+    return this.size += this.toppings;
+  }
+}
+console.log(Pizza());
+
+//user-interface logic
+
+var yourPizza = new Pizza(size, toppings);
