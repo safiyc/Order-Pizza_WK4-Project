329682b04fece78e8b0f0c9009ad322ffc1b1fa0
Safiy Cham <csafi13@yahoo.com>
2017-08-25 15:36:40 -0700

    add README.md content

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
-SPECIFICATIONS:
+# Order Pizza
+
+#### 3rd Independent Project for Epicodus
+#### Technology used: HTML, CSS, Bootstrap, JavaScript, Jquery
+
+##### By Safiy Cham
+
+## Description
+
+"Order Pizza" is a website for ordering pizzas. The customer enters the size and toppings of the pizza to get the price.
+
+## Specifications:
 
 * Program will take user's inputs of topping(s) and order size, and return it.
   * Input: Pepperoni, Pineapple, Medium
   * Output: Pepperoni, Pineapple, Medium
-
-* Program will alert user, if one or both topping(s) and size are not selected.
-  * Input: Anchovy
-  * Output: Please select toppings and size
 
 * Program will return cost after selecting a topping and size.
   * Input: Cheese, Large
@@ -14,4 +21,20 @@ SPECIFICATIONS:
 
 * Program will return cost after selecting multiple toppings and size.
   * Input: Pepperoni, Mushrooms, Small
-  * Output: $14
+  * Output: $16
+
+## Setup
+
+* Clone Github Repository
+* Open index.html on web browser
+* Enter choices and press "Submit" button
+
+## Link
+
+Click here (https://github.com/safiyc/Order-Pizza_WK4-Project) to access the Github page for this project.
+
+## License
+
+This software is licensed under the MIT licensed
+
+Copyright (c) 2017
--- js/scripts.js
+++ js/scripts.js
@@ -2,46 +2,35 @@
 function Pizza (size, toppings) {
   this.size = size;
   this.toppings = toppings;
+  this.cost = [""];
 }
 
 Pizza.prototype.getCost = function() {
-  var sizeCost = function(){
-    if (this.size === "small") {
-      return sizeCost = 10;
-    } else if (this.size === "medium") {
-      return sizeCost = 12;
-    } else {
-      return sizeCost = 15;
-    }
+  if (this.size === "small"){
+    this.cost === 8;
+  } else if (this.size === "medium"){
+    this.cost === 10;
+  } else if (this.size === "large"){
+    this.cost === 12;
   }
-
-  var toppingsCost = function(){
-    // if (this.toppings.length === 0) {
-    //   toppingsCost += 2;
-      if (this.toppings === "1") {
-        toppingsCost = 10;
-      } else if (this.toppings === "2") {
-        toppingsCost = 12;
-      } else {
-        toppingsCost = 15;
-      }
-    }
-
-    return sizeCost + toppingsCost;
-    console.log(sizeCost + toppingsCost);
-};
+  for (var i = 0; i < this.toppings.length; i++){
+    this.cost += 2;
+  }
+}
 
 // user-interface logic
 $(document).ready(function() {
   $("form").submit(function(event){
     event.preventDefault();
     var inputtedSize = $("select#size").val();
-    var inputtedToppings = $("select#toppings").val();
-    // forEach("input[name='toppings']:checked") (function(topping){
-    //   inputtedToppings.push($(this.toppings).val());
-    // });
+    var inputtedToppings = [];
+    $("input[name='toppings']:checked").each(function(){
+      var topping = $(this).val();
+      inputtedToppings.push(topping);
+    });
 
     var yourPizza = new Pizza(inputtedSize, inputtedToppings);
+    yourPizza.getCost();
     console.log(yourPizza);
 
   });
