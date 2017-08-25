ce92e4ab3f708e32ec111ba619a479f6b1313bd3
Safiy Cham <csafi13@yahoo.com>
2017-08-25 13:28:03 -0700

    add fixes to business and user-interface logics

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,48 @@
-//business logic
+// business logic
 function Pizza (size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
 
-Pizza.prototype.getCost = function () {
-  this.toppings === 2;
-  if (this.size === 8 || this.size === 12 || this.size === 15) {
-    return this.size += this.toppings;
+Pizza.prototype.getCost = function() {
+  var sizeCost = function(){
+    if (this.size === "small") {
+      return sizeCost = 10;
+    } else if (this.size === "medium") {
+      return sizeCost = 12;
+    } else {
+      return sizeCost = 15;
+    }
   }
-}
 
-//user-interface logic
+  var toppingsCost = function(){
+    // if (this.toppings.length === 0) {
+    //   toppingsCost += 2;
+      if (this.toppings === "1") {
+        toppingsCost = 10;
+      } else if (this.toppings === "2") {
+        toppingsCost = 12;
+      } else {
+        toppingsCost = 15;
+      }
+    }
 
-var yourPizza = new Pizza(size, toppings);
+    return sizeCost + toppingsCost;
+    console.log(sizeCost + toppingsCost);
+};
+
+// user-interface logic
+$(document).ready(function() {
+  $("form").submit(function(event){
+    event.preventDefault();
+    var inputtedSize = $("select#size").val();
+    var inputtedToppings = $("select#toppings").val();
+    // forEach("input[name='toppings']:checked") (function(topping){
+    //   inputtedToppings.push($(this.toppings).val());
+    // });
+
+    var yourPizza = new Pizza(inputtedSize, inputtedToppings);
+    console.log(yourPizza);
+
+  });
+});
