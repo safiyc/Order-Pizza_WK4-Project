75648c0e9332137c2ceafb7b050d93be3ae22844
safiy cham <csafi13@yahoo.com>
2017-09-01 18:20:22 -0700

    add more styling and fix logic functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,54 @@
-// business logic
-function Pizza (size, toppings) {
-  this.size = size;
-  this.toppings = toppings;
-  this.cost = [""];
-}
+// // business logic
+// function Pizza (a, b) {
+//   this.size = a;
+//   this.topping = b;
+//   this.totalCost = 0;
+// }
+//
+// Pizza.prototype.sizeCost = function() {
+//   this.sizeCost = 0;
+//   if (this.size === "small"){
+//     this.sizeCost = 6;
+//   } else if (this.size === "medium"){
+//     this.sizeCost = 8;
+//   } else if (this.size === "large"){
+//     this.sizeCost = 10;
+//   }
+// }
+//
+// Pizza.prototype.toppingCost = function() {
+//     this.toppingCost = 0;
+//     if (this.topping === "meat") {
+//       this.toppingCost += 2;
+//     } else if (this.topping === "vegetable") {
+//       this.toppingCost += 1;
+//     }
+//   }
+//
+// Pizza.prototype.getCost = function() {
+//   this.totalCost =  sizeCost() + toppingCost();
+// }
 
-Pizza.prototype.getCost = function() {
-  if (this.size === "small"){
-    this.cost === 8;
-  } else if (this.size === "medium"){
-    this.cost === 10;
-  } else if (this.size === "large"){
-    this.cost === 12;
-  }
-  for (var i = 0; i < this.toppings.length; i++){
-    this.cost += 2;
-  }
-}
 
 // user-interface logic
 $(document).ready(function() {
-  $("#pizza-form").submit(function(event){
+  $("#pizza-selection").submit(function(event){
     event.preventDefault();
     $("#pizza-options").hide();
     var inputtedSize = $("select#size").val();
     var inputtedToppings = [];
-    $("input[name='toppings']:checked").each(function(){
+    $("input[name='topping']:checked").each(function(){
       var topping = $(this).val();
       inputtedToppings.push(topping);
     });
 
-    var yourPizza = new Pizza(inputtedSize, inputtedToppings);
-    yourPizza.getCost();
-    console.log(yourPizza);
+    // var yourPizza = new Pizza(inputtedSize, inputtedToppings);
+    // yourPizza.getCost();
+    // console.log(yourPizza);
 
     $("#your-pizza").show();
+    $(".pizza-overlay").show();
+    $(".next-button").hide();
   });
 
   $("#address-form").submit(function(event){
